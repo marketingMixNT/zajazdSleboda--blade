8ceb50011186ba8345c0731c0ec45bc282ee7121
Marek <marekgacek45@gmail.com>
2024-02-28 09:01:53 +0100

    finish template of gallery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,58 @@
-import './bootstrap';
-import "./partials/hamburger"
-import "./partials/headerBgi"
-import "./partials/swiper"
-import "./partials/footerYear"
-import './partials/preloader'
-import './partials/observer'
+import "./bootstrap";
+import "./partials/hamburger";
+import "./partials/headerBgi";
+import "./partials/swiper";
+import "./partials/footerYear";
+import "./partials/preloader";
+import "./partials/observer";
 
-
-
-
-
-
-
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
 
 Alpine.start();
 
+// let filterItem = document.querySelector(".items-links");
+// let filteImages = document.querySelectorAll(".project-img");
 
+// window.addEventListener("load", () => {
+//     filterItem.addEventListener("click", (selectedItem) => {
+//         if (selectedItem.target.classList.contains("item-link")) {
+//             document
+//                 .querySelector(".menu-active")
+//                 .classList.remove("menu-active");
+//             selectedItem.target.classList.add("menu-active");
+//             let filterName = selectedItem.target.getAttribute("data-name");
+//             filteImages.forEach((image) => {
+//                 let filterImages = image.getAttribute("data-name");
+//                 if (filterImages == filterName || filterName == "all") {
+//                     image.style.display = "block";
+//                 } else {
+//                     image.style.display = "none";
+//                 }
+//             });
+//         }
+//     });
+// });
+let filterItem = document.querySelector(".items-links");
+let filteImages = document.querySelectorAll('.grid-item');
+
+window.addEventListener("load", () => {
+    filterItem.addEventListener("click", (selectedItem) => {
+        if (selectedItem.target.classList.contains("item-link")) {
+            document
+                .querySelector(".menu-active")
+                .classList.remove("menu-active");
+            selectedItem.target.classList.add("menu-active");
+            let filterName = selectedItem.target.getAttribute("data-name");
+            filteImages.forEach((image) => {
+                let filterImages = image.getAttribute("data-name");
+                if (filterImages == filterName || filterName == "all") {
+                    image.style.display = "block";
+                } else {
+                    image.style.display = "none";
+                }
+            });
+        }
+    });
+});
--- resources/views/pages/gallery/index.blade.php
+++ resources/views/pages/gallery/index.blade.php
@@ -8,44 +8,132 @@
 
 
 
-<div class="flex justify-center items-center mt-20 gap-6">
-    <button class="border border-black px-8 py-2">1</button>
-    <button class="border border-black px-8 py-2">2</button>
-    <button class="border border-black px-8 py-2">3</button>
-    <button class="border border-black px-8 py-2">4</button>
-</div>
+
+    {{-- <section id="projects">
+        <div class="container">
+            <!--MENU Links-->
+            <div class="items-links">
+                <div class="item-link menu-active" data-name="all">All</div>
+                <div class="item-link" data-name="1">2</div>
+                <div class="item-link" data-name="2">3</div>
+                <div class="item-link" data-name="3">4</div>
+            </div>
+            <!--FILTER IMAGE -->
+            <div class="gallery">
+                <div class="project-img" data-name="1">
+                    <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                        <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                    </a>
+                </div>
+                <div class="project-img" data-name="2">
+                    <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                        <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                    </a>
+                </div>
+                <div class="project-img" data-name="3">
+                    <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                        <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                    </a>
+                </div>
+                <div class="project-img" data-name="1">
+                    <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                        <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section> --}}
+
+    <section id="projects">
+      <div class="container">
+          <!--MENU Links-->
+          <div class="items-links">
+              <div class="item-link menu-active" data-name="all">All</div>
+              <div class="item-link" data-name="1">2</div>
+              <div class="item-link" data-name="2">3</div>
+              <div class="item-link" data-name="3">4</div>
+          </div>
+          <!--FILTER IMAGE -->
+          {{-- <div class="gallery">
+              <div class="project-image" data-name="1">
+                  <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                      <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                  </a>
+              </div>
+              <div class="project-image" data-name="2">
+                  <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                      <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                  </a>
+              </div>
+              <div class="project-image" data-name="3">
+                  <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                      <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                  </a>
+              </div>
+              <div class="project-image" data-name="1">
+                  <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                      <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                  </a>
+              </div>
+          </div> --}}
+          <div class="grid-gallery">
+            <div class="grid-item" data-name='1'>
+                <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                    <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+                </a>
+            </div>
+            <div class="grid-item" data-name='2'>
+                <a href="{{ asset('assets/images/apartments/apartment-2.jpeg') }}" class="glightbox">
+                    <img src="{{ asset('assets/images/apartments/apartment-2.jpeg') }}" alt="image" />
+                </a>
+            </div>
+            <div class="grid-item" data-name='3'>
+                <a href="{{ asset('assets/images/apartments/apartment-3.jpeg') }}" class="glightbox">
+                    <img src="{{ asset('assets/images/apartments/apartment-3.jpeg') }}" alt="image" />
+                </a>
+            </div>
+            <div class="grid-item" data-name='1'>
+                <a href="{{ asset('assets/images/apartments/apartment-4.jpeg') }}" class="glightbox">
+                    <img src="{{ asset('assets/images/apartments/apartment-4.jpeg') }}" alt="image" />
+                </a>
+            </div>
+            <div class="grid-item" data-name='3'>
+                <a href="{{ asset('assets/images/apartments/apartment-5.jpeg') }}" class="glightbox">
+                    <img src="{{ asset('assets/images/apartments/apartment-5.jpeg') }}" alt="image" />
+                </a>
+            </div>
+        </div>
+      </div>
+  </section>
 
 
- 
-  
-
-<div class="grid-gallery">
-    <div class="grid-item">
-  <a href="{{asset('assets/images/apartments/apartment-1.jpg')}}" class="glightbox">
-    <img src="{{asset('assets/images/apartments/apartment-1.jpg')}}" alt="image" />
-  </a>
-    </div>
-    <div class="grid-item">
-  <a href="{{asset('assets/images/apartments/apartment-2.jpeg')}}" class="glightbox">
-    <img src="{{asset('assets/images/apartments/apartment-2.jpeg')}}" alt="image" />
-  </a>
-    </div>
-    <div class="grid-item">
-  <a href="{{asset('assets/images/apartments/apartment-3.jpeg')}}" class="glightbox">
-    <img src="{{asset('assets/images/apartments/apartment-3.jpeg')}}" alt="image" />
-  </a>
-    </div>
-    <div class="grid-item">
-  <a href="{{asset('assets/images/apartments/apartment-4.jpeg')}}" class="glightbox">
-    <img src="{{asset('assets/images/apartments/apartment-4.jpeg')}}" alt="image" />
-  </a>
-    </div>
-    <div class="grid-item">
-  <a href="{{asset('assets/images/apartments/apartment-5.jpeg')}}" class="glightbox">
-    <img src="{{asset('assets/images/apartments/apartment-5.jpeg')}}" alt="image" />
-  </a>
-    </div>
-</div>
+    {{-- <div class="grid-gallery">
+        <div class="grid-item">
+            <a href="{{ asset('assets/images/apartments/apartment-1.jpg') }}" class="glightbox">
+                <img src="{{ asset('assets/images/apartments/apartment-1.jpg') }}" alt="image" />
+            </a>
+        </div>
+        <div class="grid-item">
+            <a href="{{ asset('assets/images/apartments/apartment-2.jpeg') }}" class="glightbox">
+                <img src="{{ asset('assets/images/apartments/apartment-2.jpeg') }}" alt="image" />
+            </a>
+        </div>
+        <div class="grid-item">
+            <a href="{{ asset('assets/images/apartments/apartment-3.jpeg') }}" class="glightbox">
+                <img src="{{ asset('assets/images/apartments/apartment-3.jpeg') }}" alt="image" />
+            </a>
+        </div>
+        <div class="grid-item">
+            <a href="{{ asset('assets/images/apartments/apartment-4.jpeg') }}" class="glightbox">
+                <img src="{{ asset('assets/images/apartments/apartment-4.jpeg') }}" alt="image" />
+            </a>
+        </div>
+        <div class="grid-item">
+            <a href="{{ asset('assets/images/apartments/apartment-5.jpeg') }}" class="glightbox">
+                <img src="{{ asset('assets/images/apartments/apartment-5.jpeg') }}" alt="image" />
+            </a>
+        </div>
+    </div> --}}
 
 
 
